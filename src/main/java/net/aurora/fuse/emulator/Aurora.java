7fae78f796a37c347805826ac351ae6ec73fee87
Josh <d197386@edu.rocwb.nl>
2017-04-09 01:33:11 +0200

    Initial commit.

---
+++ src/main/java/net/aurora/fuse/emulator/Aurora.java
@@ -0,0 +1,11 @@
+package net.aurora.fuse.emulator;
+
+/**
+ * The main singleton class holding all application items.
+ * @author Josh
+ */
+public class Aurora {
+    public static void main(String[] args) {
+        
+    }
+}
